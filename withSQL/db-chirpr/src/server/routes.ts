de613b046b6ec2c41e3a91837fde7174008e3f4f
Ben Bryant <brbryant2639@gmail.com>
2020-04-10 15:57:57 -0500

    edited routes to use chirps query methods

--- withSQL/db-chirpr/src/server/routes.ts
+++ withSQL/db-chirpr/src/server/routes.ts
@@ -3,22 +3,50 @@ import db from "./db";
 
 const router = express.Router();
 
-router.get("/api/hello", (req, res, next) => {
-  res.json("World");
-});
-
-router.get("/api/blogs", async (req, res) => {
+router.get("/api/chirps", async (req, res) => {
   try {
-    res.json(await db.Blogs.all());
+    res.json(await db.Chirps.all());
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
   }
 });
 
-router.get("/api/blogs/:id", async (req, res) => {
+router.get("/api/chirps/:id", async (req, res) => {
   try {
-    res.json((await db.Blogs.one(req.params.id))[0]);
+    let id = req.params.id;
+    res.json((await db.Chirps.one(id))[0]);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.post("/api/chirps", async (req, res) => {
+  try {
+    let body = req.body;
+    res.json(await db.Chirps.insert(body));
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.put("/api/chirps/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    let body = req.body.content;
+    res.json(await db.Chirps.update(id, body));
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+});
+
+router.delete("/api/chirps/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    res.json((await db.Chirps.remove(id))[0]);
   } catch (e) {
     console.log(e);
     res.sendStatus(500);
